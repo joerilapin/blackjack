6e00f89a0cd3b88f56ba06945f272ae09eac8865
joerilapin <joerilapin>
2020-02-14 11:00:30 +0100

    Explanation from Tim about his code

--- blackjack.php
+++ blackjack.php
@@ -1,34 +1,30 @@
 
 <?php
-class Blackjack
-{
-    // property declaration
-    public $var = 'a default value';
 
-    // method declaration
-    public function displayVar() {
-        echo $this->var;
+class Blackjack {
+    //score will keep track of the score at all times.
+    public $score = 0;
+
+    public function firstHand(){
+        $card1 = mt_rand(1, 11);
+        $card2 = mt_rand(1, 11);
+        $this->score = $card1 + $card2;
+        return [$card1, $card2];
+    }
+    //hit function will add a randomly generated card between 1-11
+    public function hit($currentScore){
+        $randomCard = mt_rand(1, 11);
+        $this->score = $randomCard + $currentScore;
+        return $randomCard;
+    }
+
+    //End player turn and start dealer's turn
+    public function stand(){
+        echo 'It is the dealers turn. Your score is ' . $_SESSION['score'];
+    }
+
+    //Give up (Dealer wins)
+    public function surrender(){
+        echo 'You gave up, LOSER! Dealer has won!';
     }
 }
-$obj = new Blackjack();
-$obj-> displayVar();
-
-?>
-
-
-
-
-
-
-//<?php/*
-
-$eersterandom = mt_rand(1,11);
-$tweederandom = mt_rand(1,11);
-$somplayer = $eersterandom + $tweederandom;
-echo "de random eerste kaart      ".$eersterandom;
-echo "<br/>de random tweede kaart ".$tweederandom;
-echo "<br/>de eerste twee kaarten van de player ".$somplayer;
-echo ""
-*/
-//?>
-
--- game.php
+++ game.php
@@ -1,4 +1,98 @@
 
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"
+          rel="stylesheet"/>
+    <title>Order food & drinks</title>
+</head>
+<body>
+
 <?php
-    echo "so far so good";
-    ?>
+//Require the blackjack file for access to class
+require 'blackjack.php';
+//Start session for storage of variables
+session_start();
+
+//Initiate class in player variable
+$player = new Blackjack();
+//Initiate class in dealer variable
+$dealer = new Blackjack();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    //if button on home page is pushed
+    if (isset($_POST['start'])){
+        //Players first two cards
+        $firstHand = $player->firstHand();
+        echo '<p>You have ' . $firstHand[0] . ' and a ' . $firstHand[1] . '</p>';
+        $_SESSION['playerScore'] = $player->score;
+        echo $_SESSION['playerScore'];
+        //Dealers first two cards
+        $dealerFirstHand = $dealer->firstHand();
+        $_SESSION['dealerScore'] = $dealer->score;
+    }
+
+    //If player presses hit button
+    if (isset($_POST['hit'])){
+        //In hit we create random card, add this with current score which is score. We return the generated card.
+        $hitPlayer = $player->hit($_SESSION['playerScore']);
+        echo 'Your card is ' . $hitPlayer . '<br>';
+
+        //Check if player has more then 21
+        if ($player->score > 21){
+            echo 'You lose!';
+        }
+        $_SESSION['playerScore'] += $hitPlayer;
+    }
+
+    //If player presses stands
+    if (isset($_POST['stand'])){
+        while ($dealer->score <= 15){
+            $hitDealer = $dealer->hit($dealer->score);
+            echo 'The dealer hit ' . $hitDealer . '<br>';
+            $_SESSION['dealerScore'] += $hitDealer;
+        }
+
+        //Check if dealer is over 21
+        if ($_SESSION['dealerScore'] > 21){
+            echo 'Dealer lost, You win!';
+        }
+
+        //Compare the scores
+        if ($_SESSION['playerScore'] <= $_SESSION['dealerScore']){
+            echo 'Dealer wins!';
+        } else {
+            echo 'You win!';
+        }
+    }
+
+    //If player surrenders
+    if (isset($_POST['surrender'])){
+        echo 'You lost!';
+    }
+}
+
+?>
+<div class='container'>
+    <p>Player score is: <?php echo $_SESSION['playerScore']?></p>
+</div>
+
+<form method="POST">
+    <button name="hit" type="submit" class="btn btn-primary">HIT</button>
+    <button name="stand" type="submit" class="btn btn-primary">STAND</button>
+    <button name="surrender" type="submit" class="btn btn-primary">SURENDER</button>
+</form>
+
+<form method="post" action="index.php">
+    <button name="home" type="submit" class="btn btn-primary">HOME</button>
+</form>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
